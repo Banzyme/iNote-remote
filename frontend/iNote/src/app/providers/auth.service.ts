843106f1ca9a6a1469db1ed7143646c7da6ad534
endeesa <nbanzyme@gmail.com>
2018-11-08 05:40:32 +0200

    add auth service

--- frontend/iNote/src/app/providers/auth.service.ts
+++ frontend/iNote/src/app/providers/auth.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user: Observable<firebase.User>;
+  currentUser: firebase.User;
+  isLoggedIn = false;
 
-  constructor() { }
+
+  constructor(private af_auth: AngularFireAuth, private afs: AngularFirestore, private router: Router) {
+    this.user = this.af_auth.authState;
+  }
+
+  googleSignIn() {
+    return this.af_auth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(res => {
+        if (!res.user.emailVerified) {
+          // todo: send email verification link
+        }
+        this.router.navigate(['']);
+      })
+      .catch(error => {
+        // this.err_msg = error.message;
+        // this.openDialog();
+      });
+  }
+
+
+  logout() {
+    this.isLoggedIn = false;
+    this.af_auth
+      .auth
+      .signOut()
+      .then(() => { this.router.navigate(['']) })
+  }
+
 }
