dc60b4d9586f13d488ff0059aed8f0201c900701
endeesa <nbanzyme@gmail.com>
2018-11-10 01:11:33 +0200

    Update home page layout

--- frontend/iNote/src/app/views/overview/overview.component.ts
+++ frontend/iNote/src/app/views/overview/overview.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewComponent implements OnInit {
   today = new Date();
+  time_1 = new Date();
+  time_2 = new Date();
+  time_3 = new Date();
+
+  events = [
+    { title: 'Interview with Isazi', dateTime: this.time_1, venue: 'Skype', attending: 'J Math'  },
+    { title: 'Morning scrums', dateTime: this.time_2, venue: 'Rivonia' ,attending: 'G HUnt'   },
+    { title: 'Finish assessment', dateTime: this.time_3, venue: 'Home' ,attending: 'N Ndou'   },
+
+  ]
 
   constructor() { }
 
